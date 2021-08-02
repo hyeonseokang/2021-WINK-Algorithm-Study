7b49aa1255f0c169654f6d73a4b8d891d9187be9
hyunseo <hansx2079@naver.com>
2021-08-03 02:14:30 +0900

    1주차 이름 수정

--- week1/1.py
+++
@@ -1,8 +0,0 @@
-# 프로그래머스 폰켓몬 문제
-# https://programmers.co.kr/learn/courses/30/lessons/1845?language=python3
-
-def solution(nums):
-    num_set = set(nums)
-    answer = min(len(nums) // 2, len(num_set))
-    
-    return answer
